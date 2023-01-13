3dd2cfe921517f00bbf0aba245505efbd0efdba5
DennsHartstock <dennis.hartstock@web.de>
2023-01-13 08:35:06 +0100

    add getSelectedDate()

--- app/src/main/java/com/commcode/ageinminutescalculator/MainActivity.kt
+++ app/src/main/java/com/commcode/ageinminutescalculator/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.commcode.ageinminutescalculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonSelectDate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+
+        buttonSelectDate.setOnClickListener {
+            getSelectedDate()
+        }
+    }
+
+    private fun getSelectedDate() {
+        val calendar: Calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(
+            this,
+            { _, _, _, _ ->
+                Toast.makeText(this, "getSelectedDate works", Toast.LENGTH_SHORT).show()
+            },
+            year,
+            month,
+            dayOfMonth
+        ).show()
+    }
+
+    private fun initViews() {
+        buttonSelectDate = findViewById(R.id.buttonSelectDate)
     }
 }
